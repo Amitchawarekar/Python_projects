24444e43c99468a8dea43b7585c230b67e982505
Amit Chawarekar <54398300+Amitchawarekar@users.noreply.github.com>
2021-08-17 18:19:16 +0530

    Update main.py

--- Attendance Management System using Face Recognition/main.py
+++ Attendance Management System using Face Recognition/main.py
@@ -15,32 +15,33 @@ class Face_Recognition_System:
     def __init__(self,root):
         self.root = root
         self.root.geometry("1530x790+0+0")
+        self.root.state('zoomed')
         self.root.title("Face Recognition System")
 
 
         #First Image
-        img = Image.open("images/admin_register_heading.jpg")
-        img = img.resize((500,130),Image.ANTIALIAS)
+        img = Image.open("images/attendance-heading3.jpg")
+        img = img.resize((500,150),Image.ANTIALIAS)
         self.photoimg = ImageTk.PhotoImage(img)
 
         f_lbl = Label(self.root,image = self.photoimg)
-        f_lbl.place(x=0, y=0, width=500, height=130)
+        f_lbl.place(x=0, y=0, width=500, height=150)
 
         #Second Image
-        img1= Image.open("images/admin_register_heading.jpg")
-        img1= img1.resize((550, 130), Image.ANTIALIAS)
+        img1= Image.open("images/attendance-heading4.jpg")
+        img1= img1.resize((550, 150), Image.ANTIALIAS)
         self.photoimg1 = ImageTk.PhotoImage(img1)
 
         f_lbl = Label(self.root, image=self.photoimg1)
-        f_lbl.place(x=500, y=0, width=550, height=130)
+        f_lbl.place(x=500, y=0, width=550, height=150)
 
         #Third Image
-        img2 = Image.open("images/admin_register_heading.jpg")
-        img2= img2.resize((500, 130), Image.ANTIALIAS)
+        img2 = Image.open("images/attendance-heading5.jpg")
+        img2= img2.resize((490, 150), Image.ANTIALIAS)
         self.photoimg2 = ImageTk.PhotoImage(img2)
 
         f_lbl = Label(self.root, image=self.photoimg2)
-        f_lbl.place(x=1000, y=0, width=500, height=130)
+        f_lbl.place(x=1050, y=0, width=490, height=150)
 
         # bg Image
         img3 = Image.open("images/blue-dark-gradient-texture-wall-background_28629-888.jpg")
@@ -48,9 +49,9 @@ class Face_Recognition_System:
         self.photoimg3 = ImageTk.PhotoImage(img3)
 
         bg_img = Label(self.root, image=self.photoimg3)
-        bg_img.place(x=0, y=130, width=1530, height=710)
+        bg_img.place(x=0, y=150, width=1530, height=710)
 
-        title_lbl  = Label(bg_img,text = "FACE RECOGNITION ATTENDANCE SYSTEM SOFTWARE",font= ('lucida',35,"bold"),bg='white',fg='red')
+        title_lbl  = Label(bg_img,text = " FACE RECOGNITION ATTENDANCE SYSTEM SOFTWARE",font= ('Bell MT',35,"bold"),bg='white',fg='red')
         title_lbl.place(x=0,y=0,width=1530,height=45)
 
         #================time =========================
@@ -64,7 +65,7 @@ class Face_Recognition_System:
         time()
 
         #Student Button
-        img4 = Image.open("images/student-icon.jpg")
+        img4 = Image.open("images/student_photo.jpg")
         img4 = img4.resize((220, 220), Image.ANTIALIAS)
         self.photoimg4 = ImageTk.PhotoImage(img4)
 
@@ -74,57 +75,46 @@ class Face_Recognition_System:
         b1_1 = Button(bg_img,text='Student Details',command=self.student_details, cursor='hand2',font= ('goudy',15,"bold"),bg='dark blue',fg='white')
         b1_1.place(x=200, y=300, width=220, height=40)
 
-        #Detect Face
-        img5 = Image.open("images/employee_1559924.jpg")
-        img5 = img5.resize((220, 220), Image.ANTIALIAS)
-        self.photoimg5 = ImageTk.PhotoImage(img5)
-
-        b2 = Button(bg_img, image=self.photoimg5, cursor='hand2',command=self.face_data)
-        b2.place(x=500, y=100, width=220, height=220)
-
-        b2_2 = Button(bg_img, text='Face Detector',command=self.face_data ,cursor='hand2', font=('goudy', 15, "bold"), bg='dark blue',
-                      fg='white')
-        b2_2.place(x=500, y=300, width=220, height=40)
-
-        # Attendance Report
-        img6 = Image.open("images/admin-icon.png")
-        img6 = img6.resize((220, 220), Image.ANTIALIAS)
-        self.photoimg6 = ImageTk.PhotoImage(img6)
-
-        b3 = Button(bg_img, image=self.photoimg6,command=self.attendance_data, cursor='hand2')
-        b3.place(x=800, y=100, width=220, height=220)
-
-        b3_3 = Button(bg_img, text='Attendance',command=self.attendance_data, cursor='hand2', font=('goudy', 15, "bold"), bg='dark blue',
-                      fg='white')
-        b3_3.place(x=800, y=300, width=220, height=40)
-
-        # Help Desk
-        img7 = Image.open("images/employee_bg_image.jpg")
-        img7 = img7.resize((220, 220), Image.ANTIALIAS)
-        self.photoimg7 = ImageTk.PhotoImage(img7)
-
-        b4 = Button(bg_img, image=self.photoimg7, cursor='hand2')
-        b4.place(x=1100, y=100, width=220, height=220)
-
-        b4_4 = Button(bg_img, text='Help Desk', cursor='hand2', font=('goudy', 15, "bold"), bg='dark blue',
-                      fg='white')
-        b4_4.place(x=1100, y=300, width=220, height=40)
-
-
         #Train Face button
-        img8 = Image.open("images/student-boy-sitting-in-keyboard-with-laptop-vector-28086007.jpg")
+        img8 = Image.open("images/training-image.png")
         img8 = img8.resize((220, 220), Image.ANTIALIAS)
         self.photoimg8 = ImageTk.PhotoImage(img8)
 
         b5 = Button(bg_img, image=self.photoimg8, cursor='hand2',command=self.train_data)
-        b5.place(x=200, y=400, width=220, height=220)
+        b5.place(x=500, y=100, width=220, height=220)
 
         b5_5 = Button(bg_img, text='Train Data', cursor='hand2',command=self.train_data, font=('goudy', 15, "bold"), bg='dark blue',
                       fg='white')
-        b5_5.place(x=200, y=600, width=220, height=40)
+        b5_5.place(x=500, y=300, width=220, height=40)
+
+
+        #Detect Face
+        img5 = Image.open("images/Learn-Facial-Recognition-scaled.jpg")
+        img5 = img5.resize((220, 220), Image.ANTIALIAS)
+        self.photoimg5 = ImageTk.PhotoImage(img5)
+
+        b2 = Button(bg_img, image=self.photoimg5, cursor='hand2',command=self.face_data)
+        b2.place(x=800, y=100, width=220, height=220)
+
+        b2_2 = Button(bg_img, text='Face Detector',command=self.face_data ,cursor='hand2', font=('goudy', 15, "bold"), bg='dark blue',
+                      fg='white')
+        b2_2.place(x=800, y=300, width=220, height=40)
+
+        # Attendance Report
+        img6 = Image.open("images/attendance.jpg")
+        img6 = img6.resize((220, 220), Image.ANTIALIAS)
+        self.photoimg6 = ImageTk.PhotoImage(img6)
+
+        b3 = Button(bg_img, image=self.photoimg6,command=self.attendance_data, cursor='hand2')
+        b3.place(x=1100, y=100, width=220, height=220)
+
+        b3_3 = Button(bg_img, text='Attendance',command=self.attendance_data, cursor='hand2', font=('goudy', 15, "bold"), bg='dark blue',
+                      fg='white')
+        b3_3.place(x=1100, y=300, width=220, height=40)
+
 
         # Photos button
-        img9 = Image.open("images/student-boy-sitting-in-keyboard-with-laptop-vector-28086007.jpg")
+        img9 = Image.open("images/photos_gallery.png")
         img9 = img9.resize((220, 220), Image.ANTIALIAS)
         self.photoimg9 = ImageTk.PhotoImage(img9)
 
@@ -135,29 +125,19 @@ class Face_Recognition_System:
                       fg='white')
         b9_9.place(x=500, y=600, width=220, height=40)
 
-        # Developer button
-        img10 = Image.open("images/student-boy-sitting-in-keyboard-with-laptop-vector-28086007.jpg")
-        img10 = img10.resize((220, 220), Image.ANTIALIAS)
-        self.photoimg10 = ImageTk.PhotoImage(img9)
-
-        b10 = Button(bg_img, image=self.photoimg10, cursor='hand2')
-        b10.place(x=800, y=400, width=220, height=220)
-
-        b10_10 = Button(bg_img, text='Developer', cursor='hand2', font=('goudy', 15, "bold"), bg='dark blue',
-                      fg='white')
-        b10_10.place(x=800, y=600, width=220, height=40)
-
         # exit button
-        img11 = Image.open("images/student-boy-sitting-in-keyboard-with-laptop-vector-28086007.jpg")
+        img11 = Image.open("images/exit.png")
         img11 = img11.resize((220, 220), Image.ANTIALIAS)
         self.photoimg11 = ImageTk.PhotoImage(img11)
 
-        b11 = Button(bg_img, image=self.photoimg11,command=self.exit ,cursor='hand2')
-        b11.place(x=1100, y=400, width=220, height=220)
+        b11 = Button(bg_img, image=self.photoimg11, command=self.exit, cursor='hand2')
+        b11.place(x=800, y=400, width=220, height=220)
 
-        b11_11 = Button(bg_img, text='Exit', cursor='hand2',command=self.exit,font=('goudy', 15, "bold"), bg='dark blue',
-                      fg='white')
-        b11_11.place(x=1100, y=600, width=220, height=40)
+        b11_11 = Button(bg_img, text='Exit', cursor='hand2', command=self.exit, font=('goudy', 15, "bold"),
+                        bg='dark blue',
+                        fg='white')
+        b11_11.place(x=800, y=600, width=220, height=40)
+
 
 
     #=======Functions for photos button================
